8db80fb78560e52255f98436390c8c6befa6c4ae
dhruv-p-prajapati <dhruv.prajapati@bacancy.com>
2024-08-20 18:33:19 +0530

    feat: add shadcn, router, lazy loading, loader and sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import { RouterProvider } from "react-router-dom";
+import Routes from "./routes/Routes";
+import { Suspense } from "react";
+import Loading from "./components/common/Loading";
 
 const App = () => {
-  return <div className="">Weather App</div>;
+  const router = Routes();
+  return (
+    <Suspense fallback={<Loading />}>
+      <RouterProvider router={router} />
+    </Suspense>
+  );
 };
 
 export default App;
---
+++ src/types/Linkitem.types.ts
@@ -0,0 +1,5 @@
+export interface ILinkItem {
+  slug: string;
+  label: string;
+  icon: React.ReactNode;
+}
